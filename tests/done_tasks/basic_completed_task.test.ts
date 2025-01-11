0c156ecfbe18d21ba531a510bf829ce83c9fd4c4
Evan Fedorov <evnfdrv@gmail.com>
2025-01-11 11:24:34 +0300

    completed task interface & basic comleted tasks

---
+++ tests/done_tasks/basic_completed_task.test.ts
@@ -0,0 +1,27 @@
+
+import { CompletedTask } from '../../source/comleted_tasks/comleted_task';
+import { BasicCompletedTask } from '../../source/comleted_tasks/basic_comleted_task';
+import { assert } from "chai";
+
+
+describe (
+	"BasicCompletedTask",
+
+	() => {
+		const taskName = "Do stuff";
+		const taskDate = new Date ('1968-05-17T01:20:30');
+		let aCompletedTask: CompletedTask | undefined;
+
+		it ("Construction", function () {
+			aCompletedTask = new BasicCompletedTask (taskName, taskDate);
+		});
+
+		it ("Get name of the completed task", function () {
+			assert.equal (aCompletedTask!.getName(), taskName);
+		});
+
+		it ("Get comletion date of a task", function () {
+			assert.equal (aCompletedTask!.getCompletionDate(), taskDate);
+		})
+	}
+);
